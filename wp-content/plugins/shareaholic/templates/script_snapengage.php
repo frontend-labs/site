a49e52a0892f19e57e7c6fea518077da67a3ee90
Frontend Labs <joejansanchez@gmail.com>
2014-12-15 16:06:22 -0500

    Actualizando cache.

--- wp-content/plugins/shareaholic/templates/script_snapengage.php
+++
@@ -1,9 +0,0 @@
-<!-- begin SnapEngage code -->
-<script type="text/javascript">
-  (function() {
-    var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
-    se.src = '//commondatastorage.googleapis.com/code.snapengage.com/js/62fa2e8b-38a9-4304-ba5c-86503444d30c.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
-  })();
-</script>
-<!-- end SnapEngage code -->
