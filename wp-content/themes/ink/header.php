fda039fd645ef78f91c528dc04f0ef4f05fb8fd8
Jan Sanchez <joejansanchez@gmail.com>
2015-02-17 10:54:31 -0500

    feat(header): cambiando el codigo de GA

--- wp-content/themes/ink/header.php
+++ wp-content/themes/ink/header.php
@@ -16,6 +16,22 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59791791-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+
+</script>
+<!-- End of Google Analytics -->
+
+<?php /*
 <!-- Google Analytics -->
 <script type="text/javascript">
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -29,6 +45,7 @@ ga('send', 'pageview');
 
 </script>
 <!-- End of Google Analytics -->
+*/ ?>
 
 <?php wp_head(); ?>
 </head>
