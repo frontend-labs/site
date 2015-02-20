b8f5f4bfc37508a5b1d0fe4f2773e2285bdefa9c
Jan Sanchez <joejansanchez@gmail.com>
2015-02-19 21:39:57 -0500

    Update footer.php

--- wp-content/themes/ink/footer.php
+++ wp-content/themes/ink/footer.php
@@ -23,7 +23,12 @@
 
 			<div class="after_all">
 
-				
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- after_all -->
+				<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-9151106315507816" data-ad-slot="1949147488"></ins>
+				<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>	
 
 			</div>
 
