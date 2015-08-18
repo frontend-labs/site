bb17f29c27cf98e640db02010a6c0c8b36aeb31d
Frontend Labs <frontendlabsperu@gmail.com>
2015-08-18 13:25:22 -0400

    feat(images): agrego imagenes de agosto

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 include('wp-env.php');
 define('WP_CACHE', true); //Added by WP-Cache Manager
-define( 'WPCACHEHOME', '/var/www/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/var/www/frontendlabs.io/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 $env = unserialize(ENV);
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
